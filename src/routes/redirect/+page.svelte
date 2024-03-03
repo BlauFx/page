fa543955623dbad09a4600edb4207e5ded4c9ab6
BlauFx <blaufxsup@gmail.com>
2024-03-03 01:27:02 +0100

    Add a redirect page

---
+++ src/routes/redirect/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+    import { browser } from '$app/environment';
+
+	let info = [
+		{text: "ask", url: "https://dontasktoask.com"},
+		{text: "hello", url: "https://nohello.net/en/"},
+		{text: "try", url: "https://tryitands.ee"}]
+
+	const param = $page.url.searchParams;
+	const str = param.toString().slice(0, -1);
+	for (const element in info) {
+		if (info[element].text == str) {
+			if (browser) {
+			    window.location.href = info[element].url;
+			}
+			break;
+		}
+	}
+
+</script>
+
+<p>I will list here some useful links for interacting with people. In case I get the feeling someone wastes my or even their own time with useless stuff. </p>
+
+{#each info as {text, url}, _}
+    {text}: <a href={text}>{url}</a>
+    <br>
+{/each}
+
+<br> how to use: /redirect?name
