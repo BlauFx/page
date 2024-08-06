fefc966ad87a43423f6f994ddc8e4ab8228d8260
BlauFx <blaufxsup@gmail.com>
2024-08-06 22:47:00 +0200

    Update text

--- src/routes/Text.svelte
+++ src/routes/Text.svelte
@@ -1,7 +1,10 @@
 Hey there! I'm BlauFx, 20 y/o.<br>
-I try to use my time as efficiently/productively as possible, which sadly doesn't work out quite as often as I would like it to.<br>
-Nevertheless, I strive improve myself and I would like to receive criticism about things that I could improve on.<br>
 <br>
-I'm a full-time Linux enthusiast.<br>
-In my free time I play video games, develop stuff, watch anime, try to make some AMVs, or tinker around with Linux.<br>
-I am interested in a variety of areas, starting from web development, cybersecurity, games, reverse engineering over to kernel development.<br>
+I strive improve myself and I would like to receive criticism about things that I could improve on.<br>
+I'm a full-time Linux enthusiast. I study computer science and mathematics at University Bielefeld, Germany.<br>
+<br>
+In my free time I play video games, like to develop, watch anime, try to make some AMVs, or tinker around with Linux.<br>
+I am interested in a variety of areas, starting from games, cybersecurity, reverse engineering over to kernel development.<br>
+<br>
+
+Just for fun I would like to mention an interesting philosophy is Stoicism and the Kaizen principles.
